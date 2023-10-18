1891cb55851ca3c1aa9f35522e182c2da1669e4c
khalil-kouroghli <mohamedkhalil.kouroghli@gmail.com>
2023-10-18 18:00:25 +0100

    payment

--- server/router/payment.routes.js
+++ server/router/payment.routes.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-const { getAll, add ,getById} = require("../controller/payment.controller");
-
-
-
-router.get("/getAll", getAll);
-router.get("/getFor", getById);
-router.post("/add", add);
+// const { getAll, add ,getById} = require("../controller/payment.controller");
 
 
 
+// router.get("/getAll", getAll);
+// router.get("/getFor", getById);
+// router.post("/add", add);
 
-module.exports = router;
+
+
+
+// module.exports = router;
